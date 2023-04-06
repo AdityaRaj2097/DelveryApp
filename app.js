299ea1549f652c0ea6d9afcd2da1f1dd685bee66
AdityaRaj <aditya.kumar@gopaisa.in>
2023-04-06 14:40:20 +0530

    RenderCoponent by different ways

--- app.js
+++ app.js
@@ -16,7 +16,34 @@ const heading1=React.createElement("h1",{
     key:"2",
     root:"divcontainer with ssr"
   },[heading1,heaading2])
-  
+  // console.log({heading1})
+  const Headercomponent=()=>{
+    return(
+      <>
+First way of writing compoennet
 
+      </>
+    )
+  }
+  const Headercomponent1=function(){
+    return(
+      <>
+second way of writing compoennet
+
+      </>
+    )
+  }
+   
+
+  console.log({Headercomponent})
+  // it was not Working  recheck and tell us 
+  const Headercomnpoent2 = () =>{
+    <h1>This is the Third way of Writing the function but its not working checking   </h1>
+
+  }
+
+
+  //  do frpm here
   const root=ReactDOM.createRoot(document.getElementById("root"));
-  root.render(container)
+  root.render(Headercomnpoent2)  
+  root.render(<Headercomnpoent2></Headercomnpoent2>)  
